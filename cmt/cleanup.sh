6212e864f4399758398ccec28e549dff6694f3bc
jchiang <>
2008-05-04 22:05:29 +0000

    remove CMT-generated files

--- cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/slac/g/glast/applications/CMT/v1r18p20061003; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=pgwave -version=v1 -path=/a/sulky36/g.glast.u33/tosti/myPgwave $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
